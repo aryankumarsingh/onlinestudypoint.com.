d58a6184e014ac208aab56d340679cea633b19c4
aryankumarsingh <73516576+aryankumarsingh@users.noreply.github.com>
2020-12-11 01:42:04 +0530

    Add files via upload

---
+++ js/resp.js
@@ -0,0 +1,17 @@
+burger= document.querySelector('.burger')
+navbar= document.querySelector('.navbar')
+navList= document.querySelector('.nav-list')
+
+
+
+
+burger.addEventListener('click',()=>{
+    
+
+    navList.classList.toggle('v-class');
+    navbar.classList.toggle('h-nav');
+
+
+
+})
+
